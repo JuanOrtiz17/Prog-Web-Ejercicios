b833d281caff1fc633046570bb819a38c50d17e6
Juan <juandedioo_21@alu.uabcs.mx>
2024-11-24 18:42:39 -0700

    Jo

---
+++ Ejercicio-php-db/src/views/layouts/header.php
@@ -0,0 +1,36 @@
+<header>
+        <div class="navegacion logo">
+            <img class="logo-image" src="img/phos2.png">
+        </div>
+
+        <nav class="navegacion nav-bar">
+            <a class="nav-link" href="#">
+                <span onclick="location.href='index.html';" class="ali">Manga</span><span class="express">Plus</span>
+            </a>
+        </nav>
+
+        <div class="container">
+            <input type="text" placeholder="Buscar...">
+            <button class="Boton-buscar"><i class="fa-solid fa-magnifying-glass"></i></button>
+
+            <button2><i class="fa-solid fa-bell"></i></button2>
+            <button2><i class="fa-solid fa-cart-shopping"></i></button2>
+
+            <div class="menu-container">
+                <button class="menu-button"><i class="fa-solid fa-bars"></i></button>
+            
+                <ul class="submenu">
+                    <li><i class="fa-solid fa-clipboard-list"></i> <a2 href="#">Mis pedidos</a2></li>
+                    <li><i class="fa-solid fa-heart"></i> <a2 href="#">Lista de deseos</a2></li>
+                    <li><i class="fa-solid fa-credit-card"></i> <a2 href="#">Pagos</a2></li>
+                    <li><i class="fa-solid fa-money-bill"></i> <a2 href="#">Membresías</a2></li>
+                    <li><i class="fa-solid fa-comments"></i> <a2 href="#">Centro de mensajes</a2></li>
+                </ul>
+            </div>
+
+            <button2><i class="fa-solid fa-user"></i></button2>
+            <nav>
+                <a class="nav-link2" href="#">Identificate / Registrate</a>
+            </nav>
+        </div>
+    </header>
